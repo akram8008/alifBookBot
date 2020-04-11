93258eb2e5a97b45debbb17e8f303538d99052ee
Akram <900658008.akram@gmail.com>
2020-04-11 07:01:44 +0500

    WORK5

--- betypes/config.go
+++ betypes/config.go
@@ -2,7 +2,7 @@ package betypes
 
 const (
 	BotToken       = "1012592320:AAF-lrzdvbCvGXuZFrprgJYusRJ1kuB0gHc"
-	BotWebhook     = "https://alifbookapi.herokuapp.com/"
+	BotWebhook     = "https://alifbookapi.herokuapp.com"
 	TelegramUrl    = "https://api.telegram.org/bot"
 	EnvHost        = "HOST"
 	EnvPort        = "PORT"
