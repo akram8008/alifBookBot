7e8dca8b91b4b8a7417c61d86c5b29b8c2e29a89
AkramAzamov <58902906+akram8008@users.noreply.github.com>
2021-05-19 01:26:59 +0500

    Update config.go

--- betypes/config.go
+++ betypes/config.go
@@ -1,15 +1,18 @@
 package betypes
 
 const (
-	BotToken       = "1012592320:AAF-lrzdvbCvGXuZFrprgJYusRJ1kuB0gHc"
 	BotWebhook     = "https://alifbookapi.herokuapp.com"
 	EnvPort        = "PORT"
 	AdminChatId    = 461795511
 	AdminFirstName = ""
 	AdminPhone     = ""
 	AdminRole      = "admin"
-	MySecretKey    = "secretKey"
 	LibraryUrl     = "https://git-webclient.herokuapp.com/api/enter/%s"
 	LibraryName    = "StoreBook"
 )
+
+
+
+
+
 
